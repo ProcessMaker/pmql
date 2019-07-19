235a647a7a6b990f1430408d32b85a94b83e26a9
Taylor Dondich <taylor@dondich.com>
2019-07-19 10:02:57 -0700

    Update to also support laravel 5.8. Remove composer lock.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,7 +15,7 @@ class TestCase extends \Orchestra\Testbench\TestCase
         $app['config']->set('database.default', 'testing');
     }
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
         $this->loadMigrationsFrom(__DIR__ . '/migrations');
